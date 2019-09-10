13802770ec52b74ed12fdc322fa279a076106865
mayee007 <yelekeri@gmail.com>
2019-09-09 21:10:04 -0400

    code rewritten

--- vars/JavaPipeline.groovy
+++ vars/JavaPipeline.groovy
@@ -1,56 +1,64 @@
-pipeline {
-    // Run every 15 minutes, even if there is no commits
-    triggers {
-        cron('H/15 * * * *')
-    } 
-    //properties([pipelineTriggers([cron('* * * * *')])])
+def call(body) {
 
-    environment { 
-        gitRepo = 'https://github.com/mayee007/info.git'
-        gitBranch = 'master'
-        buildCmd = 'mvn clean package -e checkstyle:checkstyle -Dspring.profiles.active=dev'
-    }
-    agent { node { label 'dockerserver' } }  
-    stages {
-        stage('checkout') {
-            steps {
-                // cleanup workspace before each build 
-                step([$class: 'WsCleanup'])
-                
-                // checkout a branch of github repository 
-                git branch : "${gitBranch}", url : "${gitRepo}"
-                
-                // list all files in workspace
-                sh 'ls -lrt'
-            }
-        }
-        stage('build') {
-            steps {
-                echo "Performing build for Build ID ${env.BUILD_ID}"
-                sh "${buildCmd}"
-            }
-        }
-        stage('Analysis') {
-            steps {
-                echo 'Performing Analysis' 
-                sh 'hostname'
-            }
-        }
-        stage('Deploy') {
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config 
+	body() 
+	
+	pipeline {
+		// Run every 15 minutes, even if there is no commits
+		triggers {
+			cron('H/15 * * * *')
+		} 
+		//properties([pipelineTriggers([cron('* * * * *')])])
 
-                    when {
-                        branch 'dev'    //only run these steps on the master branch
-                    }
-                    steps {
-                        echo 'Dev Deploy' 
-                    }
+		environment { 
+			gitRepo = 'https://github.com/mayee007/info.git'
+			gitBranch = 'master'
+			buildCmd = 'mvn clean package -e checkstyle:checkstyle -Dspring.profiles.active=dev'
+		}
+		agent { node { label 'dockerserver' } }  
+		stages {
+			stage('checkout') {
+				steps {
+					// cleanup workspace before each build 
+					step([$class: 'WsCleanup'])
+					
+					// checkout a branch of github repository 
+					git branch : "${gitBranch}", url : "${gitRepo}"
+					
+					// list all files in workspace
+					sh 'ls -lrt'
+				}
+			}
+			stage('build') {
+				steps {
+					echo "Performing build for Build ID ${env.BUILD_ID}"
+					sh "${buildCmd}"
+				}
+			}
+			stage('Analysis') {
+				steps {
+					echo 'Performing Analysis' 
+					sh 'hostname'
+				}
+			}
+			stage('Deploy') {
 
-        } // end of deploy stage 
-    } // end of stages 
-    
-    post {
-        failure {    // notify speicific users when build fails
-            mail(to: 'a@a.com', subject: "Failed Pipeline", body: "Issue with build")
-        }
-    }
+						when {
+							branch 'dev'    //only run these steps on the master branch
+						}
+						steps {
+							echo 'Dev Deploy' 
+						}
+
+			} // end of deploy stage 
+		} // end of stages 
+		
+		post {
+			failure {    // notify speicific users when build fails
+				mail(to: 'a@a.com', subject: "Failed Pipeline", body: "Issue with build")
+			}
+		}
+	}
 }
